1679c10019d5bb38a5143556b3ebf2524e8280d7
Kirill Tsukanov <ktsukanov@ebi.ac.uk>
2020-02-19 13:14:31 +0000

    Adjusted GNU Parallel options

--- vep_mapping_pipeline/run_consequence_mapping.sh
+++ vep_mapping_pipeline/run_consequence_mapping.sh
@@ -6,7 +6,12 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Explanation of GNU parallel options:
+# --halt now,fail=1: if any job fails, kill the remaining ones immediately and report failure
+# --pipe: input is read from STDIN and split by chunks
+# -j 20: number of concurrent workers
+# -N 200: number of records (lines) per worker
 bcftools query "$1" -f '%CHROM:%POS:%REF:%ALT\n' \
   | sort -u \
-  | parallel --pipe -j 20 -N 200 python3 $DIR/consequence_mapping.py \
+  | parallel --halt now,fail=1 --pipe -j 20 -N 200 python3 $DIR/consequence_mapping.py \
   | sort -u > "$2"
