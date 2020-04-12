2004c32532205f443c77a5eb48db6be5d7118411
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2020-04-12 12:38:26 +0100

    Set up package structure for pytest

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vep-mapping-pipeline',
+    version='',
+    packages=['vep_mapping_pipeline', 'repeat_expansion_variants', 'repeat_expansion_variants.test'],
+    url='',
+    license='',
+    author='ktsukanov',
+    author_email='ktsukanov@ebi.ac.uk',
+    description=''
+)
