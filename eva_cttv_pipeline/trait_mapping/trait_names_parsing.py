a52b7f3fcbbcae0160574b77e4e3d9b5dafc2e33
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2020-05-11 10:26:44 +0100

    Rewrite trait mapping pipeline to use TSV summary

--- eva_cttv_pipeline/trait_mapping/trait_names_parsing.py
+++ eva_cttv_pipeline/trait_mapping/trait_names_parsing.py
@@ -2,67 +2,35 @@ import gzip
 import json
 
 
-def clinvar_jsons(filepath: str) -> dict:
-    """
-    Yields a dict object, parsed from a file with one json per line of ClinVar records
-
-    :param filepath: String giving the path to a gzipped file containing jsons of ClinVar records,
-                     one per line.
-    :return: Yields a dictionary for each json.
-    """
-    with gzip.open(filepath, "rt") as f:
-        for line in f:
-            line = line.rstrip()
-            yield json.loads(line)
-
-
-def get_trait_names(clinvar_json: dict) -> list:
-    """
-    Given a dictionary for a ClinVar record parse the strings for the trait name of each trait in
-    the record, prioritising trait names which are specified as being "Preferred". Return a list of
-    there strings.
-
-    :param clinvar_json: A dict object containing a ClinVar record
-    :return: List of strings, the elements being one trait name for each trait in the input record.
-    """
-    # This if-else block is due to the change in the format of the CellBase JSON that holds the
-    # ClinVar data. Originally "clinvarSet" was the top level, but this level was removed and
-    # referenceClinVarAssertion is now the top level.
-    if "clinvarSet" in clinvar_json:
-        trait_set = clinvar_json["clinvarSet"]["referenceClinVarAssertion"]["traitSet"]
-    else:
-        trait_set = clinvar_json["referenceClinVarAssertion"]["traitSet"]
-    trait_names_to_return = []
-    for trait in trait_set['trait']:
-        trait_name = None
-        for name in trait['name']:
-            # First trait name in the list will always be the "Preferred" one
-            if name['elementValue']['type'] == 'Preferred':
-                trait_name = name['elementValue']['value']
-                break
-            elif name['elementValue']['type'] in ["EFO URL", "EFO id", "EFO name"]:
-                continue  # if the trait name not originally from clinvar
-            elif trait_name is None:
-                trait_name = name['elementValue']['value']
-        if trait_name is not None:
-            trait_names_to_return.append(trait_name.lower())
-
-    return trait_names_to_return
-
-
 def parse_trait_names(filepath: str) -> list:
     """
-    For a file containing ClinVar records in the format of one json per line, return a list of the
-    trait names for the records in the file.
+    For a file containing ClinVar records in the TSV format, return a list of the trait names for the records in the
+    file.
 
-    :param filepath: String giving the path to a gzipped file containing jsons of ClinVar records,
-                     one per line.
-    :return: List (important that it is a list and not a set, it is used to calculate the
-             frequencies) containing the trait names of the traits of the records in the file
-             containing ClinVar records.
+    :param filepath: Path to a gzipped file containing ClinVar TSV summary.
+    :return: A list of traits. It is important that it is a list and not a set, as it is used to calculate the
+        frequencies of each trait later on.
     """
+
+    # This is a dict of sets we use to keep track on which alleles are linked to which traits. The reason this is done
+    # is because some (most) alleles appear twice in the document with coordinates for GRCh37 and GRCh38, and we don't
+    # want to count them twice.
+    trait_names_by_allele_id = dict()
+
+    with gzip.open(filepath, "rt") as clinvar_summary:
+        header = clinvar_summary.readline().rstrip().split('\t')
+        for line in clinvar_summary:
+            line = line.rstrip().split('\t')
+            data = dict(header, values)
+            # Extract allele ID and list of phenotypes
+            allele_id = data['#Allele']
+            traits = set(data['PhenotypeList'].split(';'))
+            trait_names_by_allele_id.setdefault(allele_id, set())
+            trait_names_by_allele_id[allele_id] |= traits
+
+    # Now combine all traits into a single list
     trait_name_list = []
-    for clinvar_json in clinvar_jsons(filepath):
-        new_trait_names = get_trait_names(clinvar_json)
-        trait_name_list.extend(new_trait_names)
-    return trait_name_list
+    for traits in trait_names_by_allele_id.values():
+        trait_name_list.extend(traits)
+
+    return [t.lower() for t in trait_name_list]
