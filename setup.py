229703134b9646118e9807f45de479a7c3055f90
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2020-10-29 00:14:21 +0000

    Update configuration files

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requires():
 
 
 setup(name='eva_cttv_pipeline',
-      version='0.1',
+      version='2.0.0b1',
       packages=find_packages(),
       install_requires=get_requires(),
       package_data={
