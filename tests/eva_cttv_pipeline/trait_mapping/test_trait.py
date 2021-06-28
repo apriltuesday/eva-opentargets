85139ebe347f47724870210cf3f69a23fab87c39
Anna Foix <afoix@ebi.ac.uk>
2021-06-28 14:45:29 +0100

    remove test classes where they weren't necessary

--- tests/eva_cttv_pipeline/trait_mapping/test_trait.py
+++ tests/eva_cttv_pipeline/trait_mapping/test_trait.py
@@ -3,54 +3,56 @@ import eva_cttv_pipeline.trait_mapping.zooma as zooma
 import eva_cttv_pipeline.trait_mapping.oxo as oxo
 
 
-class TestTrait:
-    def test_is_finished_true(self):
-        test_trait = trait.Trait('aprt deficiency, japanese type', 1)
-        test_trait.finished_mapping_set.add(trait.OntologyEntry('http://www.orpha.net/ORDO/Orphanet_976',
-                                                                'Adenine phosphoribosyltransferase deficiency'))
-        assert test_trait.is_finished
+def test_is_finished_true():
+    test_trait = trait.Trait('aprt deficiency, japanese type', 1)
+    test_trait.finished_mapping_set.add(trait.OntologyEntry('http://www.orpha.net/ORDO/Orphanet_976',
+                                                            'Adenine phosphoribosyltransferase deficiency'))
+    assert test_trait.is_finished
 
-    def test_is_finished_false(self):
-        test_trait = trait.Trait('aprt deficiency, japanese type', 1)
-        assert not test_trait.is_finished
 
-    def test_process_zooma_result(self):
-        test_trait = trait.Trait('aprt deficiency, japanese type', 1)
+def test_is_finished_false():
+    test_trait = trait.Trait('aprt deficiency, japanese type', 1)
+    assert not test_trait.is_finished
 
-        test_zooma_result = zooma.ZoomaResult(['http://www.orpha.net/ORDO/Orphanet_976'],
-                                              'Adenine phosphoribosyltransferase deficiency',
-                                              'HIGH', 'eva-clinvar')
-        entry = test_zooma_result.mapping_list[0]
-        entry.confidence = zooma.ZoomaConfidence.HIGH
-        entry.in_efo = True
-        entry.is_current = True
-        entry.ontology_label = "Adenine phosphoribosyltransferase deficiency"
-        entry.source = 'eva-clinvar'
-        entry.uri = 'http://www.orpha.net/ORDO/Orphanet_976'
 
-        test_trait.zooma_result_list.append(test_zooma_result)
+def test_process_zooma_result():
+    test_trait = trait.Trait('aprt deficiency, japanese type', 1)
 
-        test_trait.process_zooma_results()
-        assert 1 == len(test_trait.finished_mapping_set)
+    test_zooma_result = zooma.ZoomaResult(['http://www.orpha.net/ORDO/Orphanet_976'],
+                                          'Adenine phosphoribosyltransferase deficiency',
+                                          'HIGH', 'eva-clinvar')
+    entry = test_zooma_result.mapping_list[0]
+    entry.confidence = zooma.ZoomaConfidence.HIGH
+    entry.in_efo = True
+    entry.is_current = True
+    entry.ontology_label = "Adenine phosphoribosyltransferase deficiency"
+    entry.source = 'eva-clinvar'
+    entry.uri = 'http://www.orpha.net/ORDO/Orphanet_976'
 
-    def test_process_oxo_mappings(self):
-        test_trait = trait.Trait('congenital cystic disease of liver', 11)
+    test_trait.zooma_result_list.append(test_zooma_result)
 
-        test_oxo_result = oxo.OxOResult('HP:0006706', 'Cystic liver disease', 'HP:0006706')
+    test_trait.process_zooma_results()
+    assert 1 == len(test_trait.finished_mapping_set)
 
-        test_oxo_mapping_1 = oxo.OxOMapping('Isolated polycystic liver disease', 'Orphanet:2924', 2,
-                                            'HP:0006706')
-        test_oxo_mapping_1.in_efo = True
-        test_oxo_mapping_1.is_current = True
 
-        test_oxo_mapping_2 = oxo.OxOMapping('cystic liver disease', 'EFO:1001505', 1, 'HP:0006706')
-        test_oxo_mapping_2.in_efo = True
-        test_oxo_mapping_2.is_current = True
+def test_process_oxo_mappings():
+    test_trait = trait.Trait('congenital cystic disease of liver', 11)
 
-        test_oxo_result.mapping_list = [test_oxo_mapping_1, test_oxo_mapping_2]
+    test_oxo_result = oxo.OxOResult('HP:0006706', 'Cystic liver disease', 'HP:0006706')
 
-        test_trait.oxo_result_list.append(test_oxo_result)
+    test_oxo_mapping_1 = oxo.OxOMapping('Isolated polycystic liver disease', 'Orphanet:2924', 2,
+                                        'HP:0006706')
+    test_oxo_mapping_1.in_efo = True
+    test_oxo_mapping_1.is_current = True
 
-        test_trait.process_oxo_mappings()
+    test_oxo_mapping_2 = oxo.OxOMapping('cystic liver disease', 'EFO:1001505', 1, 'HP:0006706')
+    test_oxo_mapping_2.in_efo = True
+    test_oxo_mapping_2.is_current = True
 
-        assert 1 == len(test_trait.finished_mapping_set)
+    test_oxo_result.mapping_list = [test_oxo_mapping_1, test_oxo_mapping_2]
+
+    test_trait.oxo_result_list.append(test_oxo_result)
+
+    test_trait.process_oxo_mappings()
+
+    assert 1 == len(test_trait.finished_mapping_set)
--- tests/eva_cttv_pipeline/trait_mapping/test_trait_names_parsing.py
+++ tests/eva_cttv_pipeline/trait_mapping/test_trait_names_parsing.py
@@ -3,12 +3,10 @@ import os.path
 import eva_cttv_pipeline.trait_mapping.trait_names_parsing as trait_names_parsing
 
 
-class TestGetTraitNames:
-
-    def test_trait_names_parsing(self):
-        # Test file contains two records: one with a Pathogenic variant another with a Benign one. Trait names
-        # from *both* records must be parsed and returned.
-        test_filename = os.path.join(os.path.dirname(__file__),
-                                     '../evidence_string_generation/resources/test_clinvar_record.xml.gz')
-        trait_names = [trait.name for trait in trait_names_parsing.parse_trait_names(test_filename)]
-        assert trait_names == ['leber congenital amaurosis 13']
+def test_trait_names_parsing():
+    # Test file contains two records: one with a Pathogenic variant another with a Benign one. Trait names
+    # from *both* records must be parsed and returned.
+    test_filename = os.path.join(os.path.dirname(__file__),
+                                 '../evidence_string_generation/resources/test_clinvar_record.xml.gz')
+    trait_names = [trait.name for trait in trait_names_parsing.parse_trait_names(test_filename)]
+    assert trait_names == ['leber congenital amaurosis 13']
