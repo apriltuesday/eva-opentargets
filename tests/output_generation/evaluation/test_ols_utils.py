e26ade7296ed5b58ed7022f13252e55a698ed8b9
April Shen <ashen@ebi.ac.uk>
2023-05-11 16:50:13 +0100

    add test for OLS query

---
+++ tests/output_generation/evaluation/test_ols_utils.py
@@ -0,0 +1,13 @@
+from cmat.output_generation.evaluation.ols_utils import fetch_eval_data
+
+
+def test_fetch_eval_data():
+    expected = ('MONDO:0004975', False, {'MONDO:0004975', 'Orphanet:238616'})
+    assert fetch_eval_data(db_iden=('MONDO', 'MONDO:0004975')) == expected
+    assert fetch_eval_data(uri='http://purl.obolibrary.org/obo/MONDO_0004975') == expected
+
+
+def test_fetch_eval_data_include_neighbors():
+    expected = ('MONDO:0004975', False, {'MONDO:0004975', 'Orphanet:238616'},
+                {'EFO:0005815', 'MONDO:0001627'}, {'MONDO:0100087', 'EFO:1001870'})
+    assert fetch_eval_data(db_iden=('MONDO', 'MONDO:0004975'), include_neighbors=True) == expected
